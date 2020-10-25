240728c53bba3507525eb17a67ad017f2b52c562
Eugene Goncharenko <36198455+evg-goncharenko@users.noreply.github.com>
2020-10-25 16:43:40 +0300

    Update comments

--- ht_2.c
+++ ht_2.c
@@ -1,48 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* Программа должна вывести самую длинную строку */
+/* The program should output the longest string */
 int main()
 {
-    /* numb - длина текущей строки, ch - текущий символ */
-	int max_len = 0, numb = 0, size = 0, ch = 0, i;
-    char* tmp = NULL; /* текущая строка */
-	char* res = NULL; /* самая длинная строка */
-	
-	while(ch != EOF){
-        while (((ch = getchar()) != '\n') && (ch != EOF)){
-            if(numb == size){
+    /* numb - length of the current string, ch - current character */
+    int max_len = 0, numb = 0, size = 0, ch = 0, i;
+    char* tmp = NULL; /* current line */
+    char* res = NULL; /* the longest line */
+    
+    while(ch != EOF)
+    {
+        while (((ch = getchar()) != '\n') && (ch != EOF))
+        {
+            if(numb == size)
+            {
                 tmp = realloc(tmp, 2*size+1);
                 size = 2*size + 1;
             }
             tmp[numb] = ch;
             numb++;
         }
-        if(numb > max_len){
+        if(numb > max_len)
+        {
             max_len = numb;
             res = realloc(res, size);
-            for(i = 0; i < numb; i++){
+            for(i = 0; i < numb; i++)
+            {
                 res[i] = tmp[i];
             }
         }
-        if(tmp != NULL){
+        if(tmp != NULL)
+        {
             free(tmp);
             tmp = NULL;
         }
         numb = size = 0;
-	}
+    }
     
-    if(res != NULL){
+    if(res != NULL)
+    {
         printf("\nThe longest line: ");
-        for(i = 0; i < max_len; i++){
+        for(i = 0; i < max_len; i++)
+        {
             printf("%c", res[i]);
         }
         printf("\n");
         
         free(res);
-    } else {
+    }
+    else
+    {
         printf("\nNo lines\n");
     }
-    
-	return 0;
+    return 0;
 }
