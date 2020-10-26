a7ab5948e80ef51c5059e7dbfe93f719f8a80acd
Eugene Goncharenko <36198455+evg-goncharenko@users.noreply.github.com>
2020-10-26 17:21:03 +0300

    Update comments

--- hw_09_22.c
+++ hw_09_22.c
@@ -4,29 +4,29 @@
 #define N 1024
 
 /*
-    Функция возвращает n-e число Фибоначчи
+    The function returns the nth Fibonacci number
 */
  int fibonacci(int n);
 
 /*
-    Функция определяет является ли число простым
+    The function determines whether a number is prime
 */
 int prime(int x);
 
 /*
-    Функция печатает все простые числа Фибоначчи, меньшие n
+    The function prints all Fibonacci primes less than n
 */
 void fibprimes(int n);
 
 /*
-    Переставляет числа в целом массиве так, чтобы вначале шли
-    отрицательные элементы (в любом порядке), затем нулевые,
-    затем положительные (также в любом порядке).
+    Rearranges the numbers of the whole array so
+    that negative elements (in any order) come first, then zero,
+    then positive (also in any order)
 */
 void remap(int a[N]);
 
 /*
-    Функция печатает все простые числа, меньшие n
+    The function prints all prime numbers less than 'n'
 */
 void primes(int n);
 
@@ -40,14 +40,19 @@ void swap(int *a, int *b)
 void remap(int a[N])
 {
     int p_neg = -1, p_zero = -1;
-    for(int i = 0; i < N; i++){
-        if(a[i] == 0){
+    for(int i = 0; i < N; i++)
+    {
+        if(a[i] == 0)
+        {
             ((p_neg + 1) > (p_zero + 1))?(p_zero = (p_neg + 1)):(p_zero++);
             swap(&a[p_zero], &a[i]);
-        } else if(a[i] < 0){
+        }
+        else if(a[i] < 0)
+        {
             p_neg++;
             swap(&a[p_neg], &a[i]);
-            if(p_zero != -1){
+            if(p_zero != -1)
+            {
                 p_zero++;
                 swap(&a[p_zero], &a[i]);
             }
@@ -55,7 +60,8 @@ void remap(int a[N])
     }
     
     printf("\nResult remap(): ");
-    for(int i = 0; i < N; i++){
+    for(int i = 0; i < N; i++)
+    {
         printf("%d ", a[i]);
     }
     printf("\n");
@@ -63,23 +69,31 @@ void remap(int a[N])
 
 void primes(int n)
 {
-    if ( n <= 1 ) {
+    if(n <= 1)
+    {
         printf("Not exist\n");
         return;
-    } else {
+    }
+    else
+    {
         printf("Result: ");
         
         int flag, tmp, numb;
-        for(int i = 2; i <= n; i++){
+        for(int i = 2; i <= n; i++)
+        {
             numb = i;
             flag = 1;
             tmp = 2;
             
-            while((tmp < pow(numb, 0.5) + 1) && (flag)){
-                if((numb % tmp == 0) && (numb != tmp)){
+            while((tmp < pow(numb, 0.5) + 1) && (flag))
+            {
+                if((numb % tmp == 0) && (numb != tmp))
+                {
                     numb /= tmp;
                     flag = 0;
-                } else{
+                }
+                else
+                {
                     tmp++;
                 }
             }
@@ -91,19 +105,24 @@ void primes(int n)
 
 void fibprimes(int n)
 {
-    if ( n <= 2 ) {
+    if(n <= 2)
+    {
         printf("Not exist\n");
         return;
-    } else {
+    }
+    else
+    {
         printf("Result: ");
  
-        int cnt = 3; /* порядковый номер ЧФ */
-        for(;;){
-            int fib = fibonacci(cnt); /* находим очередное ЧФ */
+        int cnt = 3; /* the ordinal number of the Fibonacci number */
+        for(;;)
+        {
+            int fib = fibonacci(cnt); /* find the next Fibonacci number */
  
             if(fib >= n) break;
  
-            if(prime(fib)){
+            if(prime(fib))
+            {
                 printf("%d ", fib);
             }
             cnt++;
@@ -117,22 +136,26 @@ int fibonacci(int n)
     int i;
     int fib = 0; /* F(n) */
     int n1 = 1; /* F(n + 1) или F(n - 1) */
- 
-    if (n > 0){ /* F(n) = F(n - 2) + F(n - 1) */
+    
+    if(n > 0) /* F(n) = F(n - 2) + F(n - 1) */
+    {
         if (n <= 2) return 1;
  
         int n2 = 1; /* F(n - 2) */
-        for (i = 3; i <= n; i++){
+        for (i = 3; i <= n; i++)
+        {
             fib = n2 + n1;
             n2 = n1;
             n1 = fib;
         }
- 
-    } else if(n < 0){ /* F(n) = F(n + 2) - F(n + 1) */
+    }
+    else if(n < 0) /* F(n) = F(n + 2) - F(n + 1) */
+    {
         if(n == -1) return 1;
  
-        int n2 = 0; /* F( n + 2 ) */
-        for (i = -2; i >= n; i--){
+        int n2 = 0; /* F(n + 2) */
+        for (i = -2; i >= n; i--)
+        {
             fib = n2 - n1;
             n2 = n1;
             n1 = fib;
@@ -147,34 +170,34 @@ int prime(int n)
     if (n <= 1) return 0;
  
     int tmp = 2;
-    while(tmp < pow(n, 0.5) + 1){
-        if(n % tmp == 0){ /* если делитель найден, то число непростое */
+    while(tmp < pow(n, 0.5) + 1)
+    {
+        if(n % tmp == 0) /* if the divider is found, the number isn't simple */
+        {
             n /= tmp;
             return 0;
-        } else{
+        }
+        else
+        {
             tmp++;
         }
     }
     
-    return 1; /* если делитель не найден, то число простое */
+    return 1; /* if the divisor isn't found, then the number is prime */
 }
 
 int main()
 {
-    /*
     int mass[N];
-    for(int i = 0; i < N; i++){
+    for(int i = 0; i < N; i++)
+     {
         scanf("%d", &mass[i]);
     }
     remap(mass);
-    */
     
-    /*
     primes(100);
-    */
     
-    /*
     fibprimes(50);
-    */
+    
     return 0;
 }
