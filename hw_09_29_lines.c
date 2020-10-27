64092e1a26bfaeabf2ae27ad093b2aacdef58469
Eugene Goncharenko <36198455+evg-goncharenko@users.noreply.github.com>
2020-10-27 22:11:48 +0300

    Update comments

--- hw_09_29_lines.c
+++ hw_09_29_lines.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 
-/* Приписывает t к s, возвращает s */
+/*
+    Assigns t to s, returns s:
+*/
 char* sstrcat(char *s, char *t)
 {
     char *res = s;
@@ -11,18 +13,21 @@ char* sstrcat(char *s, char *t)
     return res;
 }
 
-/* Выдает 1, если строка t расположена в конце строки s, и 0 иначе */
+/*
+    Returns 1 if the string t is located at the end of the string s, and 0 otherwise:
+*/
 int sstrend(char *s, char *t)
 {
-    for(long pos = strlen(s) - strlen(t), i = 0; pos < strlen(s); pos++, i++){
+    for(long pos = strlen(s) - strlen(t), i = 0; pos < strlen(s); pos++, i++)
+    {
         if(s[pos] != t[i]) return 0;
     }
     return (strlen(t) <= strlen(s)) ;
 }
 
 /*
-    Копирует не более n символов строки t в s, возвращает s.
-    Дополняет результат символами '\0', если символов в t меньше n
+    Copies no more than n characters of the string t to s and returns s.
+    Complements the result with '\0 ' characters if the characters in t are less than n.
 */
 char* sstrncpy(char *t, char *s, int n)
 {
@@ -33,8 +38,8 @@ char* sstrncpy(char *t, char *s, int n)
 }
 
 /*
-    Приписывает не более n символов t к s, завершая s символом '\0'.
-    Возвращает s.
+    Assigns no more than n characters t to s, ending s with '\0'.
+    Returns s.
 */
 char* sstrncat(char *s, char *t, int n)
 {
@@ -46,19 +51,19 @@ char* sstrncat(char *s, char *t, int n)
 }
 
 /*
-    Cравнивает не более n символов cs и ct.
-    Возвращает < 0, если cs < ct, 0, если cs == ct, и > 0, если cs > ct.
+    Compares not more than n characters of cs and ct.
+    Returns < 0 if cs < ct, 0 if cs == ct, and > 0 if cs > ct.
 */
 int sstrncmp(char *cs, char *ct, int n)
 {
-    for(; (n > 1) && (*cs == *ct); cs++, ct++, n--){
-        if(*cs == 0) return 0; /* когда обе строки пустые */
+    for(; (n > 1) && (*cs == *ct); cs++, ct++, n--)
+    {
+        if(*cs == 0) return 0; /* when both lines are empty */
     }
     return (*cs - *ct);
 }
 
 int main()
 {
-    
     return 0;
 }
