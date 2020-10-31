1a19ffea657ee7d5f015b3de377291d702ec3fb4
Eugene Goncharenko <gee.github@gmail.com>
2020-11-01 01:06:20 +0300

    Update comments of dictionary

--- projects/frequency_dictionary/main.c
+++ projects/frequency_dictionary/main.c
@@ -10,12 +10,12 @@
 #define OUTPUT_ERROR 2
 
 struct node {
-    char *val; /* слово */
-    int numb_occur; /* число вхождений слова */
+    char *val;      /* word */
+    int numb_occur; /* number of occurrences of a word */
     struct node *left, *right;
 }*tree;
 
-/* Печать всех слов, хранящихся в дереве: */
+/* Print all words stored in the tree: */
 void print_bin_tree(struct node *root)
 {
     if(!root) return;
@@ -25,12 +25,15 @@ void print_bin_tree(struct node *root)
     print_bin_tree(root->right);
 }
 
-/* Вставка нового элемента: */
+/* Inserting a new element: */
 struct node *add_elem_bin_tree(struct node *root, struct node *r, char *n, int *max_numb_occur)
 {
-    if(!r){
+    if(!r)
+    {
         r = (struct node*) malloc(sizeof(struct node));
-        if(!r){
+        
+        if(!r)
+        {
             fprintf(stderr, "Not enough memory!\n");
             exit(0);
         }
@@ -39,31 +42,44 @@ struct node *add_elem_bin_tree(struct node *root, struct node *r, char *n, int *
         r->numb_occur = 1;
         r->left = NULL;
         r->right = NULL;
-        if(!root) return r; /* первый вход */
-        if(strcmp(n, root->val) < 0){
+        
+        if(!root) return r; /* the first entrance */
+        
+        if(strcmp(n, root->val) < 0)
+        {
             root->left = r; /* n < root->val */
-        } else{
+        }
+        else
+        {
             root->right = r; /* n >= root->val */
         }
         return r;
     }
-    if(!strcmp(n, root->val)){ /* если такое слово уже существует */
+    
+    /* if such a word already exists */
+    if(!strcmp(n, root->val))
+    {
         r->numb_occur++;
         if((r->numb_occur) > *max_numb_occur) *max_numb_occur = (r->numb_occur);
-    } else if(strcmp(n, root->val) < 0){
+    }
+    else if(strcmp(n, root->val) < 0)
+    {
         add_elem_bin_tree(r, root->left, n, max_numb_occur); /* n < root->val */
-    } else{
+    }
+    else
+    {
         add_elem_bin_tree(r, root->right, n, max_numb_occur); /* n >= root->val */
     }
     return root;
 }
 
-/* Итоговая печать: */
+/* The final print: */
 void print_from_bin_tree(struct node *root, int max_numb_occur, int total_amount, FILE *file_out)
 {
     if(!root) return;
-    if(root->numb_occur == max_numb_occur){
-        /* frequency - частота вхождений слова */
+    
+    if(root->numb_occur == max_numb_occur)
+    {
         long double frequency = 1.0 * (root->numb_occur) / total_amount;
         fprintf(file_out, "%s %d %Lf\n", root->val, root->numb_occur, frequency);
     }
@@ -72,7 +88,6 @@ void print_from_bin_tree(struct node *root, int max_numb_occur, int total_amount
     print_from_bin_tree(root->right, max_numb_occur, total_amount, file_out);
 }
 
-/* Удаление дерева: */
 void delete_bin_tree(struct node *root)
 {
     if(!root) return;
@@ -89,66 +104,81 @@ void delete_bin_tree(struct node *root)
 int main(int argc, char **argv)
 {
     struct node *tree = NULL;
-    long total_amount = 0; /* суммарное кол-во слов */
-    int max_numb_occur = -1; /* максимальное число вхождений слова */
+    long total_amount = 0;     /* total number of words */
+    int max_numb_occur = -1;   /* maximum number of occurrences of a word */
     
-    int flag_i = 0; /* флаг на наличие опции '-i' */
-    int flag_o = 0; /* флаг на наличие опции '-o' */
-    int out_position; /* индекс имени file_out в argv */
+    int flag_i = 0;            /* flag for the '-i' option */
+    int flag_o = 0;            /* flag for the '-o' option */
+    int out_position;          /* index of the file_out name in argv */
     
     FILE *file_in;
     FILE *file_out;
     
-    /* Проверка на наличие опций: */
-    for(int i = 1; i < argc; i++){
+    /* Checking for options: */
+    for(int i = 1; i < argc; i++)
+    {
         if(!strcmp(argv[i], CONST_I)) flag_i = 1;
-        if(!strcmp(argv[i], CONST_O)){
+        
+        if(!strcmp(argv[i], CONST_O))
+        {
             flag_o = 1;
             out_position = i + 1;
         }
     }
     
-    /* Инициализация ввода: */
-    if(flag_i){
-        if ((file_in = fopen(argv[2], "r")) == NULL){
+    /* Initializing input: */
+    if(flag_i)
+    {
+        if ((file_in = fopen(argv[2], "r")) == NULL)
+        {
             fprintf(stderr, "File '%s' not found\n", argv[2]);
-            
             return INPUT_ERROR;
         }
         printf("file '%s' reading:\n", argv[2]);
-    } else{
+    }
+    else
+    {
         file_in = stdin;
         printf("stdin reading:\n");
     }
     
-    /* Инициализация вывода: */
-    if(flag_o){
-        if ((file_out = fopen(argv[out_position], "w")) == NULL){
+    /* Initializing output: */
+    if(flag_o)
+    {
+        if ((file_out = fopen(argv[out_position], "w")) == NULL)
+        {
             fprintf(stderr, "File '%s' not found\n", argv[out_position]);
-            
             return OUTPUT_ERROR;
         }
         printf("file '%s' writing:\n", argv[out_position]);
-    } else{
+    }
+    else
+    {
         file_out = stdout;
         printf("stdout writing:\n");
     }
     
-    /* Добавление элементов в дерево: */
-    char *tmp = NULL; /* слово на добавление */
-    int tmp_size = 0; /* максимальный размер массива tmp*/
-    int tmp_numb = 0; /* кол-во символов в массиве tmp */
+    /* Adding elements to the tree: */
+    char *tmp = NULL; /* word to add */
+    int tmp_size = 0; /* maximum size of the tmp array */
+    int tmp_numb = 0; /* number of characters in the tmp array */
     char c;
-    do {
+    
+    do{
         c = fgetc(file_in);
-        if ((c != '\n') && (c != ' ') && (c != EOF) && isalpha(c)){
-            if(tmp_numb == tmp_size){ /* необходимо увеличить размерность */
+        if ((c != '\n') && (c != ' ') && (c != EOF) && isalpha(c))
+        {
+            /* Need to increase the dimension: */
+            if(tmp_numb == tmp_size)
+            {
                 tmp = realloc(tmp, 2 * tmp_size + 1);
                 tmp_size = 2 * tmp_size + 1;
             }
             tmp[tmp_numb] = c;
             tmp_numb++;
-        } else if(tmp_numb){ /* пора добавлять непустое слово в дерево */
+        }
+        else if(tmp_numb) /* it's time to add a non-empty word to the tree */
+        {
             tmp[tmp_numb] = 0;
             tree = add_elem_bin_tree(tree, tree, tmp, &max_numb_occur);
             free(tmp);
@@ -156,14 +186,16 @@ int main(int argc, char **argv)
             tmp_size = tmp_numb = 0;
             total_amount++;
         }
-        /* Добавляем слова вида: ".", "-", "!", "?" ... */
-        if(!isalpha(c) && (c != '\n') && (c != ' ') && (c != EOF)){
+        /* Adding words like: ".", "-", "!", "?" ... */
+        if(!isalpha(c) && (c != '\n') && (c != ' ') && (c != EOF))
+        {
             tree = add_elem_bin_tree(tree, tree, &c, &max_numb_occur);
         }
     } while(c != EOF);
     
-    /* Печать в file_out: */
-    while(max_numb_occur > 0){
+    /* Print to file_out: */
+    while(max_numb_occur > 0)
+    {
         print_from_bin_tree(tree, max_numb_occur, total_amount, file_out);
         max_numb_occur--;
     }
