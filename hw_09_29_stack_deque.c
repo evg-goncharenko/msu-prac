ba6c3048c0588e5b3149435e5428a306276ae78e
Eugene Goncharenko <gee.github@gmail.com>
2020-11-01 00:43:26 +0300

    Update comments

--- hw_09_29_stack_deque.c
+++ hw_09_29_stack_deque.c
@@ -1,11 +1,11 @@
 /*
-    Реализация типа данных стек (фиксированного размера) с операциями:
-    - Добавление элемента;
-    - Получение элемента с вершины стека.
- 
-    Реализация типа данных дек (динамический) с операциями:
-    - Добавление элемента в начало и конец;
-    - Удаление элемента с начала и конца.
+     Implementation of the stack data type (fixed size) with operations:
+     - Add items;
+     - Getting an element from the top of the stack.
+
+     Implementation of the deque (dynamic) data type with operations:
+     - Adding an element to the beginning and end;
+     - Delete an element from the beginning and end.
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,7 +13,6 @@
 #define STACK_MAX_SIZE 20
 #define STACK_OVERFLOW  1
 #define STACK_UNDERFLOW 2
-
 #define DEQUE_OVERFLOW 3
 
 typedef struct stack_struct stack_type;
@@ -38,130 +37,159 @@ struct deque_struct {
     struct node_struct *tail;
 };
 
-/* Добавление элемента в стек: */
+/* Adding an item to the stack: */
 void stack_push(stack_type *st, stack_val_type value)
 {
-    if(st->size >= STACK_MAX_SIZE){
+    if(st->size >= STACK_MAX_SIZE)
+    {
         exit(STACK_OVERFLOW);
     }
     st->data[st->size] = value;
     st->size++;
 }
 
-/* Получение элемента с вершины стека с переходом к следующему: */
+/* Getting an item from the top of the stack and moving to the next one: */
 stack_val_type stack_pop(stack_type *st)
 {
-    if(st->size <= 0){
+    if(st->size <= 0)
+    {
         exit(STACK_UNDERFLOW);
     }
     st->size--;
     return st->data[st->size];
 }
 
-/* Получение элемента с вершины стека: */
+/* Getting an element from the top of the stack: */
 stack_val_type stack_peek(stack_type *st)
 {
-    if(st->size <= 0){
+    if(st->size <= 0)
+    {
         exit(STACK_UNDERFLOW);
     }
     return st->data[st->size - 1];
 }
 
-/* Печать стека: */
 void stack_print(stack_type *st)
 {
     int i;
     unsigned long len = st->size - 1;
     
-    if(!(st->size)){
+    if(!(st->size))
+    {
         printf("stack is empty\n");
         return;
     }
     
     printf("stack consists of %zu elements: ", st->size);
-    for(i = 0; i < len; i++){
+    
+    for(i = 0; i < len; i++)
+    {
         printf("%d | ", st->data[i]);
     }
-    if(st->size != 0){
+    
+    if(st->size != 0)
+    {
         printf("%d", st->data[i]);
     }
     printf("\n");
 }
 
-/* Добавление элемента в начало дека: */
+/* Adding an element at the beginning of the deque: */
 void deque_push_front(deque_type *d, deque_val_type v)
 {
     struct node_struct *n = malloc(sizeof(struct node_struct));
-    if(n == NULL){
+    
+    if(n == NULL)
+    {
         exit(DEQUE_OVERFLOW);
     }
     n->val = v;
     n->next = d->head;
     n->prev = NULL;
-    if(d->tail == NULL){
+    
+    if(d->tail == NULL)
+    {
         d->head = d->tail = n;
-    } else{
+    }
+    else
+    {
         d->head->prev = n;
         d->head = n;
     }
 }
 
-/* Добавление элемента в конец дека: */
+/* Adding an element to the end of the deque: */
 void deque_push_back(deque_type *d, deque_val_type v)
 {
     struct node_struct *n = malloc(sizeof(struct node_struct));
-    if(n == NULL){
+    
+    if(n == NULL)
+    {
         exit(DEQUE_OVERFLOW);
     }
     n->val = v;
     n->prev = d->tail;
     n->next = NULL;
-    if(d->head == NULL){
+    
+    if(d->head == NULL)
+    {
         d->head = d->tail = n;
-    } else{
+    }
+    else
+    {
         d->tail->next = n;
         d->tail = n;
     }
 }
 
-/* Удаление элемента с начала дека: */
+/* Deleting an element from the beginning of the deque: */
 deque_val_type deque_pop_front(deque_type *d)
 {
     deque_val_type v = d->head->val;
     struct node_struct *n = d->head;
-    if(d->head == d->tail){
+    
+    if(d->head == d->tail)
+    {
         d->head = d->tail = NULL;
-    } else{
+    }
+    else
+    {
         d->head = n->next;
     }
     free(n);
     return v;
 }
 
-/* Удаление элемента с конца дека: */
+/* Deleting an element from the end of the deque: */
 deque_val_type deque_pop_back(deque_type *d)
 {
     deque_val_type v = d->tail->val;
     struct node_struct *n = d->tail;
-    if(d->head == d->tail){
+    
+    if(d->head == d->tail)
+    {
         d->head = d->tail = NULL;
-    } else{
+    }
+    else
+    {
         d->tail = n->prev;
     }
     free(n);
     return v;
 }
 
-/* Печать дека: */
 void deque_print(deque_type *d)
 {
-    if((d->head == d->tail) && (d->head == NULL)){
+    if((d->head == d->tail) && (d->head == NULL))
+    {
         printf("deque is empty\n");
         return;
     }
     struct node_struct *n = d->head;
     printf("deque consists of: ");
-    while(n != d->tail){
+    
+    while(n != d->tail)
+    {
         printf("%d | ", n->val);
         n = n->next;
     }
@@ -170,7 +198,7 @@ void deque_print(deque_type *d)
 
 int main()
 {
-    /* Пример работы со стеком: */
+    /* Example of working with a stack: */
     stack_type stack;
     stack.size = 0;
      
@@ -188,7 +216,7 @@ int main()
     printf("stack_pop element: %d\n", stack_pop(&stack));
     stack_print(&stack);
     
-    /* Пример работы с деком: */
+    /* Example of working with a deque: */
     deque_type deque;
     printf("\n");
     
