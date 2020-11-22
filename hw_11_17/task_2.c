e065b2ad1718d6c440ccfe08f8eb7d982933ec17
Eugene Goncharenko <gee.github@gmail.com>
2020-11-22 23:52:59 +0300

    Add homework by 17.11 - 2
    The program counts the received SIGTRAP signals

---
+++ hw_11_17/task_2.c
@@ -0,0 +1,43 @@
+/*
+    The program counts the received SIGTRAP signals between
+    the 2nd and 4th pressing of CTRL+C, ends on the 7th pressing of CTRL+C
+*/
+
+#include <ctype.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <math.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int count = 1;
+void sig_handler(int s)
+{
+    count++;
+    if(count == 7)
+    {
+        signal(SIGINT, SIG_DFL);
+    }
+    else
+    {
+        if(count > 2 && count < 4)
+        {
+            signal(SIGTRAP, sig_handler);
+        }
+        else
+        {
+            signal(SIGINT, sig_handler);
+        }
+    }
+}
+
+int main()
+{
+    signal(SIGINT, sig_handler);
+    for(;;);
+    return 0;
+}
