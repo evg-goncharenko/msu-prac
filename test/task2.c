1f7ad0b91942c444dd7cce2ac574a84ed0399295
evg-goncharenko <msu.goncharenko@yandex.ru>
2020-12-08 00:03:21 +0300

    Add init commit for test

---
+++ test/task2.c
@@ -0,0 +1,18 @@
+#include <ctype.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <math.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/msg.h>
+#include <sys/ipc.h>
+#include <unistd.h>
+
+int main()
+{
+    
+    return 0;
+}
