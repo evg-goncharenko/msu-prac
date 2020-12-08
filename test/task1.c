dd4a59a2e195febd9e51cabb3b3d2ceacd86900e
evg-goncharenko <msu.goncharenko@yandex.ru>
2020-12-08 14:56:43 +0300

    Add the final solution to the first problem

--- test/task1.c
+++ test/task1.c
@@ -11,8 +11,53 @@
 #include <sys/ipc.h>
 #include <unistd.h>
 
-int main()
+/* pr1 < fname; pr2 | pr3 */
+int main(int argc, char **argv)
 {
+    int file_in = 0;
+    if(fork() == 0)
+    {
+        if((file_in = open(argv[4], O_RDONLY, 0777)) == -1)
+        {
+            perror(argv[4]);
+            exit(1);
+        }
+        
+        dup2(file_in, 0);
+        
+        execlp(argv[1], argv[1], (char*)0);
+        exit(1);
+    }
+    fflush(stdout);
+    wait(NULL);
+   
+    int fd[2];
+    pipe(fd);
+    
+    if(fork() == 0)
+    {
+        dup2(fd[1], 1);
+        close(fd[1]);
+        close(fd[0]);
+        execlp(argv[2], argv[2], (char*)0);
+        exit(1);
+    }
+    wait(NULL);
+    
+    if(fork() == 0)
+    {
+        dup2(fd[0], 0);
+        close(fd[1]);
+        close(fd[0]);
+        execlp(argv[3], argv[3], (char*)0);
+        exit(1);
+    }
+    
+    close(fd[1]);
+    close(fd[0]);
+    
+    while(wait(NULL) != -1);
     
     return 0;
 }
+
