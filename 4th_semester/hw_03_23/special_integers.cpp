48d42172a5f9a2456208a2cb21cac85f209103d1
Eugene Goncharenko <gee.github@gmail.com>
2021-03-24 18:09:13 +0300

    Fix the solution of the special_integers task
    There should be no printing in the BadRange class (there are no try-catch constructs)
    The correct name is not "mass_", but "body_"

--- 4th_semester/hw_03_23/special_integers.cpp
+++ 4th_semester/hw_03_23/special_integers.cpp
@@ -2,11 +2,9 @@
 
 class BadRange {
 private:
-    const int index;
+    int index;
 public:
-    BadRange(const int i) : index(i) {
-        std::cerr << "Error: out of range" << std::endl;;
-    }
+    BadRange(int i) : index(i) {}
     const int get() {
         return index;
     }
@@ -16,15 +14,15 @@ public:
 class SpecialInts {
     int size_;
     int capacity_;
-    int *mass_;
+    int *body_;
 public:
-    SpecialInts() : size_(0), capacity_(0), mass_(nullptr) {}
+    SpecialInts() : size_(0), capacity_(0), body_(nullptr) {}
     
     SpecialInts(int sz) {
         if (sz < 0) {
             std::cerr << "Error: vector size < 0" << std::endl;;
         } else {
-            mass_ = new int[sz];
+            body_ = new int[sz];
             size_ = sz;
             capacity_ = size_;
         }
@@ -34,12 +32,12 @@ public:
         if (sz < 0) {
             std::cerr << "Error: vector size < 0" << std::endl;;
         } else {
-            mass_ = new int[sz];
+            body_ = new int[sz];
             size_ = sz;
             capacity_ = size_;
             
             for (int i = 0; i < size_; ++i) {
-                mass_[i] = val;
+                body_[i] = val;
             }
         }
     }
@@ -47,15 +45,15 @@ public:
     SpecialInts(const SpecialInts &copy_mass) {
         size_ = copy_mass.size_;
         capacity_ = copy_mass.capacity_;
-        mass_ = new int[capacity_];
+        body_ = new int[capacity_];
         
         for (int i = 0; i < size_; ++i) {
-            mass_[i] = copy_mass.mass_[i];
+            body_[i] = copy_mass.body_[i];
         }
     }
     
     ~SpecialInts() {
-        delete [] mass_;
+        delete [] body_;
     }
     
     SpecialInts &operator , (const int &new_value) {
@@ -67,7 +65,7 @@ public:
         if (index >= size_ || index < 0 ) {
             throw BadRange(index);
         }
-        return mass_[index];
+        return body_[index];
     }
     
     SpecialInts &operator = (const SpecialInts &rhs) { // rhs = right hand side
@@ -76,14 +74,14 @@ public:
             return *this;
         }
         
-        delete [] mass_; // pre-delete the allocated memory
+        delete [] body_; // pre-delete the allocated memory
         
-        mass_ = new int[rhs.size_];
+        body_ = new int[rhs.size_];
         size_ = rhs.size_;
         capacity_ = rhs.capacity_;
 
         for (int i = 0; i < size_; ++i) {
-            mass_[i] = rhs.mass_[i];
+            body_[i] = rhs.body_[i];
         }
 
         return *this;
@@ -91,7 +89,7 @@ public:
     
     void realloc_mas() {
         if (capacity_ == 0) {
-            mass_ = new int[1];
+            body_ = new int[1];
             capacity_ = 1;
         }
         
@@ -99,25 +97,25 @@ public:
         int *tmp_mas = new int[capacity_];
         
         for (int i = 0; i < size_; ++i) {
-            tmp_mas[i] = mass_[i];
+            tmp_mas[i] = body_[i];
         }
         
-        delete [] mass_;
-        mass_ = tmp_mas;
+        delete [] body_;
+        body_ = tmp_mas;
     }
     
     void push_back(int new_elem) {
         if (size_ == capacity_) {
             realloc_mas();
         }
-        mass_[size_] = new_elem;
+        body_[size_] = new_elem;
         size_++;
     }
     
     int pop_back() {
         if (size_ > 0) {
             size_--;
-            return mass_[size_];
+            return body_[size_];
         } else {
             std::cerr << "Error: empty vector returned 0" << std::endl;;
             return 0;
@@ -134,6 +132,7 @@ int main() {
     std::cout << "Elem: " << m[0] << ", " << m[1] << ", "<< m[2];
     std::cout << ", length: " << m.len() << std::endl;
     
+    // Example of error handling:
     try {
         m[10];
     } catch (BadRange &ex){
